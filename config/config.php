22a8c74aba99e5da41273459bcbc551c9a9e9d9b
Xingshuo Cha <cedricxsa@gmail.com>
2019-10-25 02:34:00 +0800

    change

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php
+    class DB{
+        
+        static  $servername = "185.98.131.92";
+        static  $username = "5boos1240679";
+        static  $password = "260074";
+        static  $dbname = "5boos1240679";
+        
+    }
